5405dcc615fd1dd31e9fcee4c39fe5637d749dda
sotzaii_shuen <55835583shuen@gmail.com>
2021-06-20 19:26:36 +0800

    Update splan.js

--- 1.13-1.16.5/src/main/resources/splan.js
+++ 1.13-1.16.5/src/main/resources/splan.js
@@ -6,42 +6,43 @@ function initializeCoreMod() {
                 'name': 'net.minecraft.util.HTTPUtil'
             },
             'transformer': function (classNode) {
+		var SRG_Name = "func_76181_a";
+		var name = "getSuitableLanPort";
+		var desc = "(I)V";
+				
             	var asmHandler = "com/shuen/splan/splan";
-            	
+            	var asmMethodName = "getPort";
+		var asmMethodDesc = "()I";
+				
                 var Opcodes = Java.type("org.objectweb.asm.Opcodes");
-
                 var MethodInsnNode = Java.type("org.objectweb.asm.tree.MethodInsnNode");
-                var VarInsnNode = Java.type("org.objectweb.asm.tree.VarInsnNode");
-				var AbstractInsnNode = Java.type("org.objectweb.asm.tree.AbstractInsnNode");
-                var api = Java.type('net.minecraftforge.coremod.api.ASMAPI');
+		var AbstractInsnNode = Java.type("org.objectweb.asm.tree.AbstractInsnNode");
 
                 var methods = classNode.methods;
-
                 for (m in methods) 
                 {
                     var method = methods[m];
-                    if (method.name === "func_76181_a" || method.name === "getSuitableLanPort") 
+                    if (method.name === SRG_Name || method.name === name) 
                     {
                         var code = method.instructions;
                         var instr = code.toArray();
                         for (t in instr) 
                         {
                             var instruction = instr[t];
-                            if (instruction.getType() === AbstractInsnNode.METHOD_INSN && instruction.getOpcode() === Opcodes.INVOKESPECIAL && instruction.desc === "(I)V")
+                            if (instruction.getType() === AbstractInsnNode.METHOD_INSN && instruction.getOpcode() === Opcodes.INVOKESPECIAL && instruction.desc === desc)
                             {
-								var instruction2 = instruction.getPrevious();
-								if (instruction2.getOpcode() === Opcodes.ICONST_0)
-								{
-									code.remove(instruction2);
-									code.insertBefore(instruction,new MethodInsnNode(Opcodes.INVOKESTATIC, asmHandler, "getPort", "()I", false));
-								}
-        						break;
+				var instruction2 = instruction.getPrevious();
+				if (instruction2.getOpcode() === Opcodes.ICONST_0)
+					{
+						code.remove(instruction2);
+						code.insertBefore(instruction,new MethodInsnNode(Opcodes.INVOKESTATIC, asmHandler, asmMethodName, asmMethodDesc, false));
+					}
+        			break;
                             }
                         }
                         break;
                     }
                 }
-
                 return classNode;
             }
         }
